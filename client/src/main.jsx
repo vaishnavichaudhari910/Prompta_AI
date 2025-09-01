2164604963bb64815def70a0e0930f9305c77a65
Vaishnavi Jagadish Chaudhari <vaishnavichaudhari910@gmail.com>
2025-09-01 17:55:34 +0530

    project completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
   import { ClerkProvider } from '@clerk/clerk-react'
 
-
+  
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
